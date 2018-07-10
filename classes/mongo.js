eb7f499983cfbcb27a41487e946ff050b143357a
Miguel Quiambao <miguel_quiambao@dlsu.edu.ph>
2018-07-10 12:10:30 +0800

    started mongo package, insstalled required packages

--- classes/mongo.js
+++ classes/mongo.js
@@ -1,1 +1,31 @@
-import mongodb from 'mongodb';
+const mongodb = require('mongodb');
+const client = mongodb.MongoClient;
+
+module.exports = class mongo {
+    constructor () {
+        this.__mongo_ip = "localhost";
+        this.__mongo_port = "27017";
+        this.__mongo_db = "webapde";
+        
+        this.__mongo_url = "mongodb://" + this.__mongo_ip + ":" + this.__mongo_port + "/" + this.__mongo_db;
+    }
+
+    custom (json) {
+        this.__mongo_ip = ('ip' in json) ? json.ip : this.__mongo_ip;
+        this.__mongo_port = ('port' in json) ? json.port : this.__mongo_port;
+        this.__mongo_db = ('db' in json) ? json.db : this.__mongo_db;
+
+        this.__mongo_url = "mongodb://" + this.__mongo_ip + ":" + this.__mongo_port + "/" + this.__mongo_db;
+    }
+
+    
+    
+    connect () {
+        console.log("Valid DB:");
+        console.log(this.__mongo_url);
+        client.connect(this.__mongo_url, (err, db) => {
+            if (err) throw err;
+            console.log("webapde");
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
+const mongo = require('./classes/mongo');
 
 const app = express();
+
+// INITIALIZE
+const eh = new mongo();
+eh.connect();
+app.use(express.static(path.join(__dirname, 'static')));
+app.listen(3000);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/index.html'));
@@ -9,4 +16,4 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/errors/404.html'));
-})
+});
